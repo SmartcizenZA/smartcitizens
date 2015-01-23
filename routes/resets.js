b872c8b1e6c960243bc717556841dead2f309589
Ishmael Makitla <imakitla@csir.co.za>
2015-01-23 23:10:27 +0200

    Set the Action correctly on forgot.ejs, also corrected the reset password handler function for verifying the reset token. If the token is verified -exists and still valid, the password reset form is displayed

--- routes/resets.js
+++ routes/resets.js
@@ -65,6 +65,7 @@ exports.createNew = function(email, serverURL, callback){
 	  if(!err){ callback(null, userToken); }
 	  else{ callback(err);}	  
 });
+};
 //This method is used to verify that the provided token exists and is still valid
 exports.verifyToken = function(token, callback){
     Account.findOne({'passwordResetRequestToken': token, 'tokenExpiry': { $gt: Date.now() } }, function(err, account){
@@ -76,40 +77,4 @@ exports.verifyToken = function(token, callback){
 	    callback (new Error("Token Error . Your Token is either invalid or expired"));
 	   }
 	});
-};
-
-
-
-  //find the email address
-  /*
-  Account.findOne(email, function(err, user){
-	if(!user){callback (new Error("Email Address Not Found. Not Account With That Email Exists With Email Address : "+email)); return; }
-		//at this point we can now create a new request
-		var newPasswordRequest = new PasswordResetRequest({username:user.username});
-		newPasswordRequest.save(function(err){
-			if(!err){
-			  //generate the reset URL (need to get the server URL automatically)
-			  var resetURL = "<a href='http://localhost:3000/reset/'"+newPasswordRequest._id+"'>"+ "Password Reset Link: http://localhost:3000/reset/"+newPasswordRequest._id+" </a>";			  
-			  PASSWORD_RESET_BODY += resetURL;
-			  emailerUtility.sendMail(email,PASSWORD_RESET_SUBJECT,PASSWORD_RESET_BODY, function (emailError, sent){
-				if(emailError){
-				//there was a problem with emailing - indicate this to the user
-				  callback (new Error("There was a problem sending you a Password Reset Instructions via email. Error is "+emailError));
-				}
-				else{
-				  callback(null, sent);				
-				}
-			  }); 
-			}
-		});
-  });  */
-
-};
-
-//locate a password reset
-exports.getById = function (id, callback){
-  PasswordResetRequest.findById(id, function (err, resetRequest){
-	console.log ("Got PasswordResetRequest by ID = ",resetRequest);
-    callback(err, resetRequest);
-  });
 };

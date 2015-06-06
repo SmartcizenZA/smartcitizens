031e14460548f366d936a5004d94c505f71c607e
Ishmael Makitla <imakitla@csir.co.za>
2015-06-06 20:17:58 +0200

    Added Geocoding feature for the supplied coordinates

--- routes/trafficlights.js
+++ routes/trafficlights.js
@@ -2,6 +2,8 @@
   This is the script for processing incoming reports of spotting Traffic Lights from Smart Citizens;
 */
 var entities = require('../models/modelentities');
+var geocoder = require('geocoder');
+
 var TrafficLight = entities.TrafficLight;
 
 //adding a new traffic light
@@ -12,11 +14,21 @@ exports.add = function (newTrafficLightData, callback){
 				'y':newTrafficLightData.location.ycoordinates,
 				'working': newTrafficLightData.isWorking
 				};
-	var newTrafficLight = new TrafficLight(data);
-	newTrafficLight.save(function (err) {
-		if(!err){console.log("All went well. Traffic Light Successfully Added To Smart Citizen DATA Foundation. "); }
-		  callback(err, newTrafficLight);
+				
+	geocode(data.x, data.y, function(err, geoCodedString){
+	  if(!err){
+	    //got the geocoded string, add it as street1
+		data.street1 = geoCodedString;		
+	  }
+	  var newTrafficLight = new TrafficLight(data);
+		newTrafficLight.save(function (err) {
+			if(!err){console.log("All went well. Traffic Light Successfully Added To Smart Citizen DATA Foundation. "); }
+			callback(err, newTrafficLight);
+		});
+	  
 	});
+	
+	
 };
 /*
   List all traffic lights spotted
@@ -41,4 +53,25 @@ exports.getTrafficLightById = function (id, callback){
 exports.getClosestsTrafficLights = function (coordinates, callback){
 
 };
+
+/*
+   This is a utility function that geo-codes the x-y coordinates received from the spotter.
+*/
+function geocode(x, y, callback){
+  if(!x || !y){ return callback(new Error("Both X and Y MUST be submitted."));}
+  //
+  var longitute = parseFloat('-26.087212');
+  var latitute = parseFloat('27.983646');
+  
+  console.log("geocode("+longitute+" , "+latitute+")");
+  
+  geocoder.reverseGeocode( longitute, latitute, function ( err, data ) {
+		if(!err){
+		   console.log("GeoCoding Results:: ("+longitute+" , "+latitute+")", data);
+		   var geocodedAddress = (data.results && data.results[0]? data.results[0].formatted_address : "");
+		   return callback(null, geocodedAddress);
+		}
+	});
+  
+}
 
